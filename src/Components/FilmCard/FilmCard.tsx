85da902f88f1f094781702de56824be9cfa9f73a
Wintermanius <extraserzh@gmail.com>
2023-04-14 18:54:24 +0300

    видево работает

--- src/Components/FilmCard/FilmCard.tsx
+++ src/Components/FilmCard/FilmCard.tsx
@@ -1,17 +1,30 @@
-import { FC } from "react"
+import { FC, MouseEvent, useRef, useState } from "react"
 import { FilmType } from "../../types/film-type"
+import VideoPlayer from "../VideoPlayer/VideoPlayer"
 
 type FilmCardProps = {
   film: FilmType
 }
 
 const FilmCard: FC<FilmCardProps> = ({ film }) => {
-  const { title, poster, id } = film
+  const [isPlay, setIsPlay] = useState<boolean>(false)
+  const { title, poster, id, video } = film
+
+
+  const videoRef = useRef(null)
+
+  function onMouseOverHandler(e: MouseEvent) {
+    setIsPlay(true)
+  }
+
+  function onMouseOutHandler() {
+    setIsPlay(false)
+  }
 
   return (
-  <article className="small-film-card catalog__films-card" key = {id}>
+    <article onMouseOver={onMouseOverHandler} onMouseOut={onMouseOutHandler} className="small-film-card catalog__films-card" key = {id}>
       <div className="small-film-card__image">
-        <img src={poster} width="280" height="175" />
+        <VideoPlayer isPlay={isPlay} films={film} />
       </div>
       <h3 className="small-film-card__title">
         <a className="small-film-card__link" href="film-page.html">{title}</a>
@@ -21,3 +34,5 @@ const FilmCard: FC<FilmCardProps> = ({ film }) => {
 }
 
 export default FilmCard
+
+{/* <VideoPlayer films={film} /> */}
