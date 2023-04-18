6c35dbe37f31b6bb1ca55d04c0a3962ffb8f752f
Wintermanius <extraserzh@gmail.com>
2023-04-18 18:18:09 +0300

    сделал переход на страницу фильма

--- src/Components/FilmCard/FilmCard.tsx
+++ src/Components/FilmCard/FilmCard.tsx
@@ -28,7 +28,7 @@ const FilmCard: FC<FilmCardProps> = ({ film }) => {
         <VideoPlayer isPlay={isPlay} films={film} />
       </div>
       <h3 className="small-film-card__title">
-        <Link className="small-film-card__link" to="/films/:id">{name}</Link>
+        <Link className="small-film-card__link" to={`/films/${id}`}>{name}</Link>
       </h3>
     </article>
   )
--- src/Pages/MoviePage.tsx
+++ src/Pages/MoviePage.tsx
@@ -1,155 +1,161 @@
 import { FC } from "react"
 import Header from "../Components/Header/Header"
-import { Link } from "react-router-dom"
+import { Link, useParams } from "react-router-dom"
 import { FilmType } from "../types/film-type"
 
 type MoviePageProps = {
-  film: FilmType
+  films: FilmType[]
 }
 
-const MoviePage: FC<MoviePageProps> = ({film}) => {
-
-  const {backgroundImage, name, genre, released, posterImage, rating, description, director, starring} = film
+const MoviePage: FC<MoviePageProps> = ({films}) => {
+  const params = useParams()
+  console.log(params)
+  
+  const film = films.length ? films.find((film) => (film.id === Number(params.id))) : null
 
   return (
     <>
-      <section className="film-card film-card--full">
-        <div className="film-card__hero">
-          <div className="film-card__bg">
-            <img src={backgroundImage} alt={name} />
-          </div>
-
-          <h1 className="visually-hidden">WTW</h1>
-
-          <Header />
-
-          <div className="film-card__wrap">
-            <div className="film-card__desc">
-              <h2 className="film-card__title">{name}</h2>
-              <p className="film-card__meta">
-                <span className="film-card__genre">{genre}</span>
-                <span className="film-card__year">{released}</span>
-              </p>
-
-              <div className="film-card__buttons">
-                <button className="btn btn--play film-card__button" type="button">
-                  <svg viewBox="0 0 19 19" width="19" height="19">
-                    <use xlinkHref="#play-s"></use>
-                  </svg>
-                  <span>Play</span>
-                </button>
-                <button className="btn btn--list film-card__button" type="button">
-                  <svg viewBox="0 0 19 20" width="19" height="20">
-                    <use xlinkHref="#add"></use>
-                  </svg>
-                  <span>My list</span>
-                  <span className="film-card__count">9</span>
-                </button>
-                <Link to="/review" className="btn film-card__button">Add review</Link>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div className="film-card__wrap film-card__translate-top">
-          <div className="film-card__info">
-            <div className="film-card__poster film-card__poster--big">
-              <img src={posterImage} alt={name + 'poster'} width="218" height="327" />
+      {film && 
+      <>
+        <section className="film-card film-card--full">
+          <div className="film-card__hero">
+            <div className="film-card__bg">
+              <img src={film.backgroundImage} alt={film.name} />
             </div>
 
-            <div className="film-card__desc">
-              <nav className="film-nav film-card__nav">
-                <ul className="film-nav__list">
-                  <li className="film-nav__item film-nav__item--active">
-                    <a href="#" className="film-nav__link">Overview</a>
-                  </li>
-                  <li className="film-nav__item">
-                    <a href="#" className="film-nav__link">Details</a>
-                  </li>
-                  <li className="film-nav__item">
-                    <a href="#" className="film-nav__link">Reviews</a>
-                  </li>
-                </ul>
-              </nav>
+            <h1 className="visually-hidden">WTW</h1>
 
-              <div className="film-rating">
-                <div className="film-rating__score">{rating}</div>
-                <p className="film-rating__meta">
-                  <span className="film-rating__level">Very good</span>
-                  <span className="film-rating__count">240 ratings</span>
+            <Header />
+
+            <div className="film-card__wrap">
+              <div className="film-card__desc">
+                <h2 className="film-card__title">{film.name}</h2>
+                <p className="film-card__meta">
+                  <span className="film-card__genre">{film.genre}</span>
+                  <span className="film-card__year">{film.released}</span>
                 </p>
-              </div>
 
-              <div className="film-card__text">
-      
-                {description}
-
-                <p className="film-card__director"><strong>Director: {director}</strong></p>
-
-                <p className="film-card__starring"><strong>Starring: {starring[0] + ', ' + starring[1] + ', ' + starring[2]} and other</strong></p>
+                <div className="film-card__buttons">
+                  <button className="btn btn--play film-card__button" type="button">
+                    <svg viewBox="0 0 19 19" width="19" height="19">
+                      <use xlinkHref="#play-s"></use>
+                    </svg>
+                    <span>Play</span>
+                  </button>
+                  <button className="btn btn--list film-card__button" type="button">
+                    <svg viewBox="0 0 19 20" width="19" height="20">
+                      <use xlinkHref="#add"></use>
+                    </svg>
+                    <span>My list</span>
+                    <span className="film-card__count">9</span>
+                  </button>
+                  <Link to="/review" className="btn film-card__button">Add review</Link>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      </section>
 
-      <div className="page-content">
-        <section className="catalog catalog--like-this">
-          <h2 className="catalog__title">More like this</h2>
-
-          <div className="catalog__films-list">
-            <article className="small-film-card catalog__films-card">
-              <div className="small-film-card__image">
-                <img src="img/fantastic-beasts-the-crimes-of-grindelwald.jpg" alt="Fantastic Beasts: The Crimes of Grindelwald" width="280" height="175" />
+          <div className="film-card__wrap film-card__translate-top">
+            <div className="film-card__info">
+              <div className="film-card__poster film-card__poster--big">
+                <img src={film.posterImage} alt={film.name + 'poster'} width="218" height="327" />
               </div>
-              <h3 className="small-film-card__title">
-                <a className="small-film-card__link" href="film-page.html">Fantastic Beasts: The Crimes of Grindelwald</a>
-              </h3>
-            </article>
 
-            <article className="small-film-card catalog__films-card">
-              <div className="small-film-card__image">
-                <img src="img/bohemian-rhapsody.jpg" alt="Bohemian Rhapsody" width="280" height="175" />
-              </div>
-              <h3 className="small-film-card__title">
-                <a className="small-film-card__link" href="film-page.html">Bohemian Rhapsody</a>
-              </h3>
-            </article>
+              <div className="film-card__desc">
+                <nav className="film-nav film-card__nav">
+                  <ul className="film-nav__list">
+                    <li className="film-nav__item film-nav__item--active">
+                      <a href="#" className="film-nav__link">Overview</a>
+                    </li>
+                    <li className="film-nav__item">
+                      <a href="#" className="film-nav__link">Details</a>
+                    </li>
+                    <li className="film-nav__item">
+                      <a href="#" className="film-nav__link">Reviews</a>
+                    </li>
+                  </ul>
+                </nav>
 
-            <article className="small-film-card catalog__films-card">
-              <div className="small-film-card__image">
-                <img src="img/macbeth.jpg" alt="Macbeth" width="280" height="175" />
-              </div>
-              <h3 className="small-film-card__title">
-                <a className="small-film-card__link" href="film-page.html">Macbeth</a>
-              </h3>
-            </article>
+                <div className="film-rating">
+                  <div className="film-rating__score">{film.rating}</div>
+                  <p className="film-rating__meta">
+                    <span className="film-rating__level">Very good</span>
+                    <span className="film-rating__count">240 ratings</span>
+                  </p>
+                </div>
 
-            <article className="small-film-card catalog__films-card">
-              <div className="small-film-card__image">
-                <img src="img/aviator.jpg" alt="Aviator" width="280" height="175" />
+                <div className="film-card__text">
+        
+                  {film.description}
+
+                  <p className="film-card__director"><strong>Director: {film.director}</strong></p>
+
+                  <p className="film-card__starring"><strong>Starring: {film.starring[0] + ', ' + film.starring[1] + ', ' + film.starring[2]} and other</strong></p>
+                </div>
               </div>
-              <h3 className="small-film-card__title">
-                <a className="small-film-card__link" href="film-page.html">Aviator</a>
-              </h3>
-            </article>
+            </div>
           </div>
         </section>
 
-        <footer className="page-footer">
-          <div className="logo">
-            <a href="main.html" className="logo__link logo__link--light">
-              <span className="logo__letter logo__letter--1">W</span>
-              <span className="logo__letter logo__letter--2">T</span>
-              <span className="logo__letter logo__letter--3">W</span>
-            </a>
-          </div>
+        <div className="page-content">
+          <section className="catalog catalog--like-this">
+            <h2 className="catalog__title">More like this</h2>
 
-          <div className="copyright">
-            <p>© 2019 What to watch Ltd.</p>
-          </div>
-        </footer>
-      </div>
+            <div className="catalog__films-list">
+              <article className="small-film-card catalog__films-card">
+                <div className="small-film-card__image">
+                  <img src="img/fantastic-beasts-the-crimes-of-grindelwald.jpg" alt="Fantastic Beasts: The Crimes of Grindelwald" width="280" height="175" />
+                </div>
+                <h3 className="small-film-card__title">
+                  <a className="small-film-card__link" href="film-page.html">Fantastic Beasts: The Crimes of Grindelwald</a>
+                </h3>
+              </article>
+
+              <article className="small-film-card catalog__films-card">
+                <div className="small-film-card__image">
+                  <img src="img/bohemian-rhapsody.jpg" alt="Bohemian Rhapsody" width="280" height="175" />
+                </div>
+                <h3 className="small-film-card__title">
+                  <a className="small-film-card__link" href="film-page.html">Bohemian Rhapsody</a>
+                </h3>
+              </article>
+
+              <article className="small-film-card catalog__films-card">
+                <div className="small-film-card__image">
+                  <img src="img/macbeth.jpg" alt="Macbeth" width="280" height="175" />
+                </div>
+                <h3 className="small-film-card__title">
+                  <a className="small-film-card__link" href="film-page.html">Macbeth</a>
+                </h3>
+              </article>
+
+              <article className="small-film-card catalog__films-card">
+                <div className="small-film-card__image">
+                  <img src="img/aviator.jpg" alt="Aviator" width="280" height="175" />
+                </div>
+                <h3 className="small-film-card__title">
+                  <a className="small-film-card__link" href="film-page.html">Aviator</a>
+                </h3>
+              </article>
+            </div>
+          </section>
+
+          <footer className="page-footer">
+            <div className="logo">
+              <a href="main.html" className="logo__link logo__link--light">
+                <span className="logo__letter logo__letter--1">W</span>
+                <span className="logo__letter logo__letter--2">T</span>
+                <span className="logo__letter logo__letter--3">W</span>
+              </a>
+            </div>
+
+            <div className="copyright">
+              <p>© 2019 What to watch Ltd.</p>
+            </div>
+          </footer>
+        </div>
+      </>
+      }
     </>
   )
 }
