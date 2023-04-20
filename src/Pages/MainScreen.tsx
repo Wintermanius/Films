340094a36cbe487738934195551c1b3cb74a443c
Wintermanius <extraserzh@gmail.com>
2023-04-20 16:20:34 +0300

    Сделал сортировку по жанрам

--- src/Pages/MainScreen.tsx
+++ src/Pages/MainScreen.tsx
@@ -7,6 +7,7 @@ import { useStore } from "effector-react"
 import { $film, $films } from "../store/store"
 import { fetchFilmsFx, fetchOneFilmFx } from "../store/api"
 import FilmCardWrap from "../Components/FilmCardWrap/FilmCardWrap"
+import GenreList from "../Components/GenreList/GenreList"
 
 
 type MainScreenProps = {
@@ -21,53 +22,7 @@ const MainScreen: FC<MainScreenProps> = ({film1, films1}) => {
       <section className="film-card">
         {film1 && <FilmCardWrap film={film1} />}
       </section>
-
-      <div className="page-content">
-        <section className="catalog">
-          <h2 className="catalog__title visually-hidden">Catalog</h2>
-
-          <ul className="catalog__genres-list">
-            <li className="catalog__genres-item catalog__genres-item--active">
-              <a href="#" className="catalog__genres-link">All genres</a>
-            </li>
-            <li className="catalog__genres-item">
-              <a href="#" className="catalog__genres-link">Comedies</a>
-            </li>
-            <li className="catalog__genres-item">
-              <a href="#" className="catalog__genres-link">Crime</a>
-            </li>
-            <li className="catalog__genres-item">
-              <a href="#" className="catalog__genres-link">Documentary</a>
-            </li>
-            <li className="catalog__genres-item">
-              <a href="#" className="catalog__genres-link">Dramas</a>
-            </li>
-            <li className="catalog__genres-item">
-              <a href="#" className="catalog__genres-link">Horror</a>
-            </li>
-            <li className="catalog__genres-item">
-              <a href="#" className="catalog__genres-link">Kids & Family</a>
-            </li>
-            <li className="catalog__genres-item">
-              <a href="#" className="catalog__genres-link">Romance</a>
-            </li>
-            <li className="catalog__genres-item">
-              <a href="#" className="catalog__genres-link">Sci-Fi</a>
-            </li>
-            <li className="catalog__genres-item">
-              <a href="#" className="catalog__genres-link">Thrillers</a>
-            </li>
-          </ul>
-
-          <div className="catalog__films-list">
-            <FilmList films={films1}  />
-          </div>
-
-          <div className="catalog__more">
-            <button className="catalog__button" type="button">Show more</button>
-          </div>
-        </section>
-      </div>
+      <GenreList films={films1} />
     </>
   )
 }
